2132ec782f6119faa358508a9eb2a7990aaa0d94
PJ Kelly <pj@crushlovely.com>
2014-07-30 20:18:10 -0700

    Set version to 0.1.0.

--- spec/lib/aws_signer/version_spec.rb
+++ spec/lib/aws_signer/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'AwsSigner::VERSION' do
   it 'should be the correct version' do
-    expect(AwsSigner::VERSION).to eq('1.0.0')
+    expect(AwsSigner::VERSION).to eq('0.1.0')
   end
 end
